497f84c2a633305f07f5035e2830660442c0978f
manbirsingh24 <manbirfk2468@gmail.com>
2025-02-15 14:19:32 +0530

    added sidebar and main-page

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,31 @@
+import './sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className='stack'>
+            <img src="" alt="stack" />
+        </div>
+        <div className='pin'>
+            <img src="" alt="pin" />
+        </div>
+        <div className='business'>
+            <img src="" alt="business" />
+        </div>
+        <div className='building'>
+            <img src="" alt="building" />
+        </div>
+        <div className='personal'>
+            <img src="" alt="personal" />
+        </div>
+        <div className='shop'>
+            <img src="" alt="shop" />
+        </div>
+        <div className='list'>
+            <img src="" alt="list" />
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
--- src/pages/LandingPage.jsx
+++
@@ -1,7 +0,0 @@
-// import React from 'react'
-
-export default function landingPage() {
-  return (
-    <div>landingPage</div>
-  )
-}
