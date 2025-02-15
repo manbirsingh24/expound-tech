2819d114587dadbf22bd120d9cb8f8440974e2cc
manbirsingh24 <manbirfk2468@gmail.com>
2025-02-15 17:32:09 +0530

    added icons

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,43 +1,54 @@
-import React from 'react'
-import Sidebar from '../Sidebar/Sidebar.jsx'
-import Content from '../Content/Content.jsx'
-import './mainPage.css'
+import React from "react";
+import Sidebar from "../Sidebar/Sidebar.jsx";
+import Content from "../Content/Content.jsx";
+import "./mainPage.css";
+
+import done from "../../assets/images/done.svg";
+import replay from "../../assets/images/replay.svg"
+import tag from "../../assets/images/tag.svg"
+import group from "../../assets/images/group.svg"
+import list from "../../assets/images/list.svg"
 
 const MainPage = () => {
   return (
     <div className="main-page">
-        <Sidebar />
-        <div className="container">
-          <div className="header">
-            <div className="breadcrumbs">Breadcrumb</div>
-            <div className="headerInfo">
-              <div className="info-title">
-                <span>LE-1023</span>
-              </div>
-              <div className="header-icons">
-                <img src="" alt="one" />
-                <img src="" alt="two" />
-                <img src="" alt="three" />
-                <img src="" alt="four" />
-              </div>
+      <Sidebar />
+      <div className="container">
+        <div className="header">
+          <div className="breadcrumbs">Breadcrumb</div>
+          <div className="headerInfo">
+            <div className="info-title">
+              <span>LE-1023</span>
+            </div>
+            <div className="header-icons">
+              <img src={tag} alt="one" />
+              <img src="" alt="two" />
+              <img src={group} alt="three" />
+              <img src={list} alt="four" />
+              <img src={replay} alt="" />
             </div>
           </div>
-          <div className="middle">
-            <div className="chip-section">
-              <div className="complete"></div>
-              <div className="complete"></div>
-              <div className="nurturing">Nurturing</div>
-              <div className="convert">Convert</div>
-            </div>
-            <div>
-              <img src="" alt="tick" />
-              <span>Mark as Converted</span>
-            </div>
-          </div>
-          <Content/>
         </div>
+        <div className="middle">
+          <div className="chip-section">
+            <div className="complete">
+              <img src={done} alt="" />
+            </div>
+            <div className="complete">
+              <img src={done} alt="" />
+            </div>
+            <div className="nurturing">Nurturing</div>
+            <div className="convert">Convert</div>
+          </div>
+          <div>
+            <img src="" alt="tick" />
+            <span>Mark as Converted</span>
+          </div>
+        </div>
+        <Content />
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default MainPage
+export default MainPage;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,34 @@
-import './navbar.css'
+import "./navbar.css";
+import search from "../../assets/images/serach.svg";
+import calender from "../../assets/images/calendar.svg";
+import setting from "../../assets/images/setting.svg";
+import notification from "../../assets/images/notification.svg";
+
 const Navbar = () => {
   return (
     <div className="navbar">
-            <div className="title-box">
-                <div className='title'>
-                    <img src="" alt="grid-img" />
-                    <h5>Expount Technivo</h5>
-                </div>
-                <div className='search'>
-                    <input type="text" />
-                    <img src="" alt="search" />
-                </div>
-            </div>
-            <div className="navigation">
-                <img src="" alt="bell" />
-                <img src="" alt="claender" />
-                <img src="" alt="gear" />
-            </div>
-            <div className="avatar">
-                <img src="" alt="avatar" />
-            </div>
+      <div className="title-box">
+        <div className="title">
+          <img src="" alt="grid-img" />
+          <h5>Expount Technivo</h5>
+        </div>
+      </div>
+      <div className="navigation-search">
+        <div className="search">
+          <input type="text" />
+          <img src={search} alt="search" />
+        </div>
+        <div className="icons">
+          <img src={notification} alt="bell" />
+          <img src={calender} alt="claender" />
+          <img src={setting} alt="gear" />
+        </div>
+      <div className="avatar">
+        <img src="" alt="avatar" />
+      </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
